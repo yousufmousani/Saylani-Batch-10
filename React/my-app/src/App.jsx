efd64a782e4f390d85bf026add77c7ecf9ee34e5
yousuf-mousani <khananirafiq@gmail.com>
2023-12-29 14:55:37 +0500

    Use State

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,40 @@
+// UseState In React
+
+
+
+// import React, { useState } from "react";
+
+
+
+// const App = ()=>{
+//   const state = useState();
+  
+//   const  [count,setcount] = useState(0)
+  
+//   let IncNum = () =>{
+//     setcount(count+1)
+//   };
+//   let DecNum = ()=>{
+//     setcount(count -1)
+//   }
+//   let ResNum = ()=>{
+//     setcount(count = 0)
+//   }
+
+// return(
+//   <>
+//   <center>
+//     <h1>COUNTER APP</h1>
+//     <br/>
+//   <h1>{count}</h1>
+//   <br/>
+//   <button onClick={IncNum}>Increment</button>
+//   <button onClick={DecNum}>Decrement</button>
+//   <button onClick={ResNum}>Reset</button>
+//   </center>
+//   </>
+// )
+// }
+
+// export default App;
+
