8d5df19ecfde1a2c85e0b6f1b7c0fb833415cc20
yousuf-mousani <khananirafiq@gmail.com>
2024-01-01 23:01:42 +0500

    components

--- React/class-2/src/App.jsx
+++ React/class-2/src/App.jsx
@@ -4,15 +4,7 @@ import Header from './componenets/Header/Header';
 import Footer from './componenets/Header/Footer/Footer';
 
 function App() {
-  return(
-   <div style={{textAlign:'center',backgroundColor:'black',color:'white',marginTop:'100px'}}>
-   <Header/>
-   <br/>
-   <h1>Text</h1>
-   <br/>
-   <Footer/>
-   </div>
- ) 
+  
 }
 
 export default App;
