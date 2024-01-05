2ce243aae5625b7c037d3054fabd6afd51a8c151
yousuf-mousani <khananirafiq@gmail.com>
2024-01-05 14:31:45 +0500

    Assignment

---
+++ React/class-4/src/App.jsx
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+    
+  const name = "Hello World";
+  const obj = {name: "Hello World Object"}
+  const data = ['We', 'are', 'United']
+  const list = [{name: "Hello World 1"}, {name: "Hello World 2"}, {name: "Hello World 3"}]
+  const complex = [{company: 'XYZ', jobs: ['Javascript,','React']}, {company: 'ABC', jobs: ['AngularJs','Ionic']}]
+return (
+  <div style={{textAlign:'center'}}>
+      <div>{name}</div><br />
+<div>{obj.name}</div><br />
+{data.map((value,index)=>{
+  return <div style={{display:'inline-block'}}>{value}</div>
+
+  
+})}
+
+{list.map((value,index)=>{
+return <div style={{display:'block'}}>{value.name}</div>
+})}
+<table style={{border:'3px solid black',margin:'0 auto 0 auto'}}>
+{complex.map((value,index)=>{
+  return  <tr>
+      <td style={{border:'1px solid black'}}>{value.company}</td>
+      <td style={{border:'1px solid black'}}>{value.jobs}</td>
+      
+      </tr>
+  
+})}
+</table>
+
+      </div>
+  
+  
+)
+}
+
+export default App
