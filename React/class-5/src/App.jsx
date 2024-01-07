8a12b62a430f87373da2e70c65044c36e040bbff
yousuf-mousani <khananirafiq@gmail.com>
2024-01-07 15:02:19 +0500

    Bulb

--- React/class-5/src/App.jsx
+++ React/class-5/src/App.jsx
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+
+function States() {
+  let [bulb,setbulb] = useState(true)
+
+  function handle(){
+    setbulb(!bulb)
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      {
+        bulb ? <img src="https://toppng.com/public/uploads/thumbnail/light-bulb-on-off-png-11553940208gbhhx7rj3t.png" alt="" width={'300px'} />
+        : <img src="https://toppng.com/public/uploads/thumbnail/light-bulb-on-off-png-115539403195dzrdrd6di.png" alt="" width={'300px'}/>
+        
+      }
+      <button onClick={handle}>On/Off</button>
+
       </header>
     </div>
   );
 }
 
-export default App;
+
+export default States;
