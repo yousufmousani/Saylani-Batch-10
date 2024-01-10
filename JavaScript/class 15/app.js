877a7bcd2b8017a4ae874cfea5a6bd5396289499
yousuf-mousani <khananirafiq@gmail.com>
2024-01-10 12:22:25 +0500

    Fetch Api

---
+++ JavaScript/class 15/app.js
@@ -0,0 +1,8 @@
+fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(res => res.json())
+      .then(res => {
+
+       for(var key in res){
+        document.write(res[key].url +'<br/>')
+        // console.log(json)
+      }})
