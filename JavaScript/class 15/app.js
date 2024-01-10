c168de9498a3192ac3d85270d246197d6a7c2ca5
yousuf-mousani <khananirafiq@gmail.com>
2024-01-10 12:51:23 +0500

    Fetch Api

--- JavaScript/class 15/app.js
+++ JavaScript/class 15/app.js
@@ -1,8 +1,18 @@
-fetch('https://jsonplaceholder.typicode.com/photos')
-      .then(res => res.json())
-      .then(res => {
+// fetch('https://jsonplaceholder.typicode.com/photos')
+//       .then(res => res.json())
+//       .then(res => {
 
-       for(var key in res){
-        document.write(res[key].url +'<br/>')
-        // console.log(json)
-      }})
+//        for(var key in res){
+//         document.write(res[key].url +'<br/>')
+//         // console.log(json)
+//       }})
+
+import React from 'react'
+
+function app() {
+  return (
+    <div>app</div>
+  )
+}
+
+export default app
