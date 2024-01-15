37d665905e8eaa252534ff872d9fde74bb6e37f6
yousuf-mousani <khananirafiq@gmail.com>
2024-01-16 00:15:33 +0500

    Quiz App

--- React/class-6/src/App.jsx
+++ React/class-6/src/App.jsx
@@ -194,3 +194,169 @@ export default App;
 // }
 
 // export default App;
+
+
+
+
+
+// Quiz app with Score and component//
+
+
+
+
+// import React from 'react'
+// import { useState , useEffect } from 'react';
+// import Result from './components/Result';
+// // import Result from './Result'
+// let score = 0
+
+
+
+// function Quiz() {
+
+
+//   const [questions , setquestions] = useState([])
+//   const [currentindex , setindex] = useState(0)
+//   // const [click, setclick] = useState()
+//   // console.log(click,setclick);
+
+//   useEffect(()=>{
+//     getdata()
+    
+//   },[])
+
+//    let getdata = ()=>{
+//     fetch('https://the-trivia-api.com/v2/questions')
+//       .then(response => response.json())
+//       .then(res =>{
+//         res.map((item)=>{
+//           item.option = [...item.incorrectAnswers,item.correctAnswer]
+//           item.option = shuffle(item.option)
+//         }) 
+//         // 
+//      setquestions(res)
+//         // console.log(res); 
+//         }
+      
+//         )
+//    }
+
+
+// // =========================>
+
+//    function shuffle(array) {
+//     let currentIndex = array.length,  randomIndex;
+  
+//     // While there remain elements to shuffle.
+//     while (currentIndex > 0) {
+  
+//       // Pick a remaining element.
+//       randomIndex = Math.floor(Math.random() * currentIndex);
+//       currentIndex--;
+  
+//       // And swap it with the current element.
+//       [array[currentIndex], array[randomIndex]] = [
+//         array[randomIndex], array[currentIndex]];
+//     }
+  
+//     return array;
+//   }
+  
+//   // Used like so
+//   var arr = [2, 11, 37, 42];
+//   shuffle(arr);
+
+// // =========================>
+ 
+// if(!questions.length){
+//   return(
+//     <h1>Loading...</h1>
+//   )
+// }
+
+
+
+
+// function next()
+// {
+//   setindex (currentindex + 1)
+// }
+
+// function Finish(){
+//   // console.log('Your score is '+score)
+
+//   {
+//     currentindex === questions.length == 9 ?
+//     <Result scr={'your score is ' + score}/>:
+//     console.log()
+//           }
+  
+  
+// }
+
+// function check(opt){
+  
+//  opt == questions[currentindex].correctAnswer && score ++
+// //  console.log( score++  );
+// //  console.log(opt);
+
+
+
+// }
+
+
+
+//   return (
+//     <div>
+//         <h1>Quiz App</h1>
+//         <ul>  
+//            <li> {questions[currentindex].question.text}  </li>  
+//            <li> {questions[currentindex].correctAnswer} </li>
+//           </ul>
+
+
+
+// {
+
+//   questions[currentindex].option.map((data)=>{
+//     return(
+//       <div>
+//         <input type="radio" name='data' onClick={()=>check(data)} /> 
+//         <label htmlFor="">{data}</label> 
+//         <div>
+          
+//         </div>
+//       </div>
+      
+//     )
+
+//   })
+
+
+
+  
+  
+// } 
+//  {
+ 
+//         currentindex === questions.length -1  ?
+//         <button onClick={Finish} >Finish</button> :
+//          <button id='btn' onClick={next}>Next</button> 
+       
+//       }
+
+//       {
+// currentindex === questions.length-1 ?
+
+
+// <Result scr={'your score is ' + score}/>:
+// console.log()
+//       }
+
+
+       
+//     </div>
+//   )
+// }
+
+// export default Quiz;
---
+++ React/class-6/src/components/Result.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// function Result({scr}) {
+//   return (
+//     <div><h2>{scr}</h2></div>
+//   )
+// }
+
+// export default Result
